41d26c4bd0816d003df03c4395505fee70c1d7c8
Aditya <adityasharma7627@gmail.com>
2024-02-02 01:38:41 +0530

    second

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource = "/dice" + randomNumber1 + ".png";
+var randomImageSource = "./dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "./dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
